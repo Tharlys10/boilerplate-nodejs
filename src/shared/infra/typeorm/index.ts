52c580f1eb36b27294a57cb6ca70f3fbb1669804
tharlys10 <tharlys2015c@gmail.com>
2021-10-24 16:31:27 -0300

    feat: :sparkles: create migration users

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,17 @@
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (): Promise<Connection> => {
+interface IOptions {
+  host: string;
+}
+
+export default async (host = "database-boilerplate-node"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      database: process.env.NODE_ENV === 'test' ? 'boilerplate-node-test' : defaultOptions.database
+      host: process.env.NODE_ENV === 'test' ? 'localhost' : host,
+      database: process.env.NODE_ENV === 'test' ? 'boilerplate-node-test' : defaultOptions.database,
+      port: process.env.NODE_ENV === 'test' ? 9001 : 5432,
     })
   )
 }
