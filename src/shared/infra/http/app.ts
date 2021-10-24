e132240391a20bb9626805d0a4cd6f41c95c52ee
tharlys10 <tharlys2015c@gmail.com>
2021-10-24 16:12:42 -0300

    feat: :sparkles: create service db in docker and config ormconfig typeorm

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,6 +2,10 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 
+import createConnection from "@shared/infra/typeorm";
+
+createConnection();
+
 const app = express();
 
 app.use(express.json());
