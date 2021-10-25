2d676d033164fa6aae31924406c9a055812b303f
tharlys10 <tharlys2015c@gmail.com>
2021-10-25 17:40:56 -0300

    feat: :sparkles: implementation middleware de authentication

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,9 +3,11 @@ import { Router } from "express";
 import { CreateUserController } from "@modules/users/useCases/createUser/CreateUserController";
 import { UpdateUserController } from "@modules/users/useCases/updateUser/UpdateUserController";
 
+import { ensureAuthentication } from "../middlewares/ensureAuthentication";
+
 const usersRoutes = Router();
 
 usersRoutes.post("/", new CreateUserController().handle);
-usersRoutes.put("/:id", new UpdateUserController().handle);
+usersRoutes.put("/", ensureAuthentication, new UpdateUserController().handle);
 
 export { usersRoutes }
