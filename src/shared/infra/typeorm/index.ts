9d8707a87b51167c713548df5cea02972e287f19
tharlys10 <tharlys2015c@gmail.com>
2021-11-14 22:02:34 -0300

    feat: :sparkles: implementation depends_on database in docker compose

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,13 +1,17 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database-boilerplate-node"): Promise<Connection> => {
+export default async (
+  host = "database-boilerplate-node"
+): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
       host: process.env.NODE_ENV === "test" ? "localhost" : host,
-      database: process.env.NODE_ENV === "test" ? "boilerplate-node-test" : defaultOptions.database,
-      port: process.env.NODE_ENV === "test" ? 9001 : 5432,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "boilerplate-node-test"
+          : defaultOptions.database,
     })
   );
 };
